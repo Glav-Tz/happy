e7333d63163366e090869412b826b11dd2437f88
GorshkovAnton <GlavTz@gmail.com>
2023-06-28 05:29:14 +0300

    feat: обновил слайдер

--- src/components/UI/OurHeroes/data.ts
+++ src/components/UI/OurHeroes/data.ts
@@ -28,7 +28,7 @@ const categories = [
     idCategories: 6,
   },
   {
-    nameCategories: 'Куколки',
+    nameCategories: 'Соцсети',
     idCategories: 7,
   },
 ];
@@ -38,14 +38,14 @@ const hero = [
     imgCharacter: 'sweetTooth',
     characterName: 'Сладкая вечеринка',
     descShow: 'Все сладости в одной игровой программе',
-    consistsOfGroups: [0, 1],
+    consistsOfGroups: [0],
   },
   {
     imgCharacter: 'unicorn',
     characterName: 'Единорожка',
     descShow:
       'Самая нежная и волшебная программа, в которой собрано все, что любят Единороги',
-    consistsOfGroups: [0, 2],
+    consistsOfGroups: [0],
   },
   {
     imgCharacter: 'puppyPatrol',
@@ -56,28 +56,29 @@ const hero = [
   },
   {
     imgCharacter: 'spiderMan',
-    characterName: 'Человек шелкопряд',
-    descShow: 'Есть у него веретено, всех врагов он победит легко.',
-    consistsOfGroups: [0, 2],
+    characterName: 'Человек-Паук',
+    descShow:
+      'Гигантские паутины, умение лазить по стенам и другие “суперсилы” Человека Паука',
+    consistsOfGroups: [0, 4],
   },
   {
     imgCharacter: 'haggiWaggi',
     characterName: 'Хагги Вагги',
     descShow: 'Путешествие на фабрику игрушек Poppy Playtime',
-    consistsOfGroups: [0, 3],
+    consistsOfGroups: [3],
   },
   {
     imgCharacter: 'tickTok',
     characterName: 'Тик Ток',
     descShow: 'Устройте своему ребенку ТикТок челлендж',
-    consistsOfGroups: [0, 3],
+    consistsOfGroups: [0, 7, 1],
   },
   {
     imgCharacter: 'youTube2',
     characterName: 'Yotube Челленджи',
     descShow:
       'Самые популярные челленджи в стиле ВладаА4 и других известных блогеров',
-    consistsOfGroups: [1],
+    consistsOfGroups: [7, 1],
   },
   {
     imgCharacter: 'rose',
@@ -109,121 +110,121 @@ const hero = [
     characterName: '2 рыжих кота',
     descShow:
       'Какими должны быть котики? Что они любят? Малыши узнают вместе с нашими  веселыми котами',
-    consistsOfGroups: [6],
+    consistsOfGroups: [2],
   },
   {
     imgCharacter: 'antistress',
     characterName: 'антистресс-пати',
     descShow:
       'Pop-it, паззлы, головоломки, сквиши и мячики помогут весело провести время и успокоить нервы',
-    consistsOfGroups: [6],
+    consistsOfGroups: [5],
   },
   {
     imgCharacter: 'ladyBug',
     characterName: 'Леди баг',
     descShow:
       'Обожаемая всеми Леди Баг поделиться секретами своих суперспособностей',
-    consistsOfGroups: [6],
+    consistsOfGroups: [2, 4],
   },
   {
     imgCharacter: 'wednesday',
     characterName: 'Уэнздей',
     descShow: 'Кровавая вечеринка для всей семьи',
-    consistsOfGroups: [6],
+    consistsOfGroups: [0],
   },
   {
     imgCharacter: 'blogging',
     characterName: 'Блогерская вечеринка',
     descShow:
       'Познакомимся со всеми соц-сетями и узнаем, как стать    популярными в каждой из них',
-    consistsOfGroups: [6],
+    consistsOfGroups: [1],
   },
   {
-    imgCharacter: 'littleMermaid',
+    imgCharacter: 'likes',
     characterName: 'Likee-вечеринка',
     descShow:
       'Самые свежие тренды, танцевальная музыка, море лайков и позитива',
-    consistsOfGroups: [6],
+    consistsOfGroups: [7],
   },
   {
     imgCharacter: 'youTube',
     characterName: 'Yotube детский',
     descShow: 'YouTube-челленджи, адаптированные для детей от 5 до 8 лет',
-    consistsOfGroups: [6],
+    consistsOfGroups: [7],
   },
   {
     imgCharacter: 'pirates',
     characterName: 'Пиратский квест',
     descShow:
       'Кто не мечтал отправится в путешествие и отыскать настоящий пиратский клад?',
-    consistsOfGroups: [6],
+    consistsOfGroups: [5],
   },
   {
     imgCharacter: 'paleontologists',
     characterName: 'Дино-Пати',
     descShow:
       'Отыщите следы динозавров вместе с нашими веселыми исследователями',
-    consistsOfGroups: [6],
+    consistsOfGroups: [5],
   },
   {
     imgCharacter: 'pajama',
     characterName: 'Пижамная вечеринка',
     descShow: 'Бой подушками, любимые игрушки и другие ”пижамные” развлечения',
-    consistsOfGroups: [6],
+    consistsOfGroups: [5, 1],
   },
   {
     imgCharacter: 'harry',
     characterName: 'Гарри Поттер',
     descShow:
       'Погрузись в мир волшебства Хогвартс с самым известным волшебником',
-    consistsOfGroups: [6],
+    consistsOfGroups: [3, 5],
   },
   {
     imgCharacter: 'harley',
     characterName: 'Харли квин',
     descShow: 'Немного похулиганим вместе с очаровательной Харли Квин',
-    consistsOfGroups: [6],
+    consistsOfGroups: [4],
   },
   {
     imgCharacter: 'football',
     characterName: 'Футбольная вечеринка',
     descShow:
       'Стань крутым футболистом и победи команду соперников в настоящем матче',
-    consistsOfGroups: [6],
+    consistsOfGroups: [5],
   },
   {
     imgCharacter: 'squid',
     characterName: 'Игра в кальмара',
     descShow:
       'Хотите пощекотать нервы и погрузиться в атмосферу      нашумевшего сериала?',
-    consistsOfGroups: [6],
+    consistsOfGroups: [1],
   },
   {
     imgCharacter: 'mafia',
     characterName: 'Мафия',
     descShow:
       'Город засыпает, просыпается мафия... а ты сможешь вычислить опасную группировку?',
-    consistsOfGroups: [6],
+    consistsOfGroups: [1],
   },
   {
     imgCharacter: 'rainbow',
     characterName: 'Радужные друзья',
     descShow: 'Познакомься со всеми друзьями из хоррор игры от Роблокс',
-    consistsOfGroups: [6],
+    consistsOfGroups: [3],
   },
   {
     imgCharacter: 'supers',
     characterName: 'Супермен и супергерл',
     descShow:
       'Супергеройская игровая программа по мотивам любимого мультфильма',
-    consistsOfGroups: [6],
+    consistsOfGroups: [4],
   },
   {
     imgCharacter: 'Thor',
     characterName: 'Тор',
     descShow:
       'Эффектный супергерой с гигантским молотом научит управлять молниями',
-    consistsOfGroups: [6],
+    consistsOfGroups: [4],
   },
   {
     imgCharacter: 'snowWhite',
@@ -256,6 +257,13 @@ const hero = [
     imgCharacter: 'enchantimals',
     characterName: 'Данесса Олени',
     descShow: 'Отправьтесь в удивительный мир куколок Энчантималс с Данессой',
+    consistsOfGroups: [6],
+  },
+  {
+    imgCharacter: 'lol',
+    characterName: 'Куолка ЛОЛ',
+    descShow:
+      'Наряды, украшения, звездный подиум - что еще любят куколки модницы?',
     consistsOfGroups: [6],
   },
 ];
